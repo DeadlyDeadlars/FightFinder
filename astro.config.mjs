52044495f046ec7fe2827b02ead6969b72f03470
Mark Marushak <mark@example.com>
2026-01-22 10:30:20 +0500

    Add GitHub Actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://markmarushak.github.io',
-  base: '/fightfinder-landing',
+  site: 'https://DeadlyDeadlars.github.io',
+  base: '/FightFinder',
   vite: {
     plugins: [tailwindcss()]
   },
